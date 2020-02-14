d4b4e83b2de95048e82d8d5d5268b164ed0769fd
Richard McMullin <richard.e.mcmullin@gmail.com>
2020-02-14 16:59:09 -0500

    Update the shooter command

--- src/main/java/frc/robot/commands/shooter/DefaultShooterCommand.java
+++ src/main/java/frc/robot/commands/shooter/DefaultShooterCommand.java
@@ -12,58 +12,54 @@ import frc.robot.Robot;
  */
 public class DefaultShooterCommand extends TSafeCommand {
 
-	private static final String COMMAND_NAME =
-			DefaultShooterCommand.class.getSimpleName();
+    private static final String COMMAND_NAME =
+            DefaultShooterCommand.class.getSimpleName();
 
-	public DefaultShooterCommand() {
+    public DefaultShooterCommand() {
 
-		super(TConst.NO_COMMAND_TIMEOUT, Robot.oi);
+        super(TConst.NO_COMMAND_TIMEOUT, Robot.oi);
 
-		// Use requires() here to declare subsystem dependencies
-		requires(Robot.shooterSubsystem);
-	}
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.shooterSubsystem);
+    }
 
-	@Override
-	protected String getCommandName() { return COMMAND_NAME; }
+    @Override
+    protected String getCommandName() { return COMMAND_NAME; }
 
-	@Override
-	protected String getParmDesc() {
-		return super.getParmDesc();
-	}
+    @Override
+    protected String getParmDesc() {
+        return super.getParmDesc();
+    }
 
-	// Called just before this Command runs the first time
-	@Override
-	protected void initialize() {
-		// Print the command parameters if this is the current
-		// called command (it was not sub-classed)
-		if (getCommandName().equals(COMMAND_NAME)) {
-			logMessage(getParmDesc() + " starting");
-		}
-	}
-
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	protected void execute() {
-	    
-        if (Robot.oi.stopShooter()) {
-            Scheduler.getInstance().add(new StopShooterCommand());
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+        // Print the command parameters if this is the current
+        // called command (it was not sub-classed)
+        if (getCommandName().equals(COMMAND_NAME)) {
+            logMessage(getParmDesc() + " starting");
         }
-        
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+
         HoodPosition userSelectedHoodPostion = Robot.oi.getHoodPosition();
         Robot.shooterSubsystem.setHoodPosition(userSelectedHoodPostion);
-
-        double userSelectedShooterSpeed = Robot.oi.getShooterSpeed();
-        Robot.shooterSubsystem.setShooterMotorSpeed(userSelectedShooterSpeed);
 
         if (Robot.oi.runShooterBB()) {
             Scheduler.getInstance().add(new BangBangShooterCommand());
         }
+
+        double userSelectedShooterSpeed = Robot.oi.getShooterSpeed();
+        Robot.shooterSubsystem.setShooterMotorSpeed(userSelectedShooterSpeed);
     }
 
-	// Make this return true when this Command no longer needs to run execute()
-	@Override
-	protected boolean isFinished() {
-		return false;
-	}
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 
 }
